d6819e17a912562cff974bf9fa85287d43611dcb
sanda-bunescu <sanda.accounts@proton.me>
2023-11-18 17:09:56 +0200

    New Project

---
+++ LenghtConversion/LenghtConversion/LenghtConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  LenghtConversionApp.swift
+//  LenghtConversion
+//
+//  Created by Sanda Bunescu on 08.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct LenghtConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RockPaperScrissors/RockPaperScrissors/RockPaperScrissorsApp.swift
@@ -0,0 +1,11 @@
+
+import SwiftUI
+
+@main
+struct RockPaperScrissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
