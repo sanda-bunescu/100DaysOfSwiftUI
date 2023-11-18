0a41ad9711cd87d78ed82bde52b7bd32fac8f6e1
sanda-bunescu <sanda.accounts@proton.me>
2023-11-18 17:19:54 +0200

    Add BetterRest

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Sanda Bunescu on 21.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
