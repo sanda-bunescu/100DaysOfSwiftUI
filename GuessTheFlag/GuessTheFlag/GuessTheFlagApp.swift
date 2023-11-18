10fefb341d922de0df5e9aa5afb2b3cd8e0a18e5
sanda-bunescu <sanda.accounts@proton.me>
2023-11-18 16:50:34 +0200

    Add GuessTheFlag

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Sanda Bunescu on 10.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
